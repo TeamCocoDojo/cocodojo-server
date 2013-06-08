feb146e1935d2010bdfecdde23be9a5afdb5bda8
seanxiaoxiao <seanxiaoxiao@hotmail.com>
2013-06-08 00:09:55 -0700

    Tweak code

--- app.js
+++ app.js
@@ -4,15 +4,12 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , codeSession = require('./routes/code_session')
   , index = require("./routes/index")
   , http = require('http')
   , path = require('path')
   , io = require("socket.io");
 
 var ot = require("ot");
-
 var app = express();
 
 // all environments
@@ -20,14 +17,15 @@ app.set('port', process.env.PORT || 3333);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
-app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 // development only
 if ('development' == app.get('env')) {
+  app.use(express.logger('dev'));
   app.use(express.errorHandler());
 }
 
@@ -41,23 +39,34 @@ var headerMiddleware = function(req, res, next) {
 app.all('/*', headerMiddleware);
 app.get('/index.html', index.index);
 
-http.createServer(app).listen(5000, function(){
-  console.log('Express server listening on port ' + app.get('port'));
+var syncServers = {};
+
+http.createServer(app).listen(5000, function() {
+    console.log('Express server listening on port ' + app.get('port'));
 });
 
-var editorServer = null;
 var io = require('socket.io').listen(3333);
-io.set('origins', '*');
-io.sockets.on('connection', function (socket) {
+io.set('origins', process.env.origin || '*:*');
+io.sockets.on('connection', function(socket) {
 
   socket.emit('doneConnection', { message: 'hello' });
 
   socket.on('create', function(data) {
-    editorServer = new ot.EditorSocketIOServer("", [], data.codeSessionId);
-    io.sockets.emit('doneCreate', "");
+    var editorServer = new ot.EditorSocketIOServer("", [], data.codeSessionId);
+    syncServers[data.codeSessionId] = editorServer;
+    socket.emit('doneCreate', {});
   });
 
   socket.on('join', function(data) {
-    editorServer.addClient(socket);
+    console.log(data);
+    if (syncServers[data.codeSessionId]) {
+      var editorServer = syncServers[data.codeSessionId];
+      editorServer.addClient(socket);
+    }
   });
 });
+
+
+
+
+
--- conf.js
+++
@@ -1,8 +0,0 @@
-var conf = {
-  db: {
-    db: 'cocodojo',
-    host: 'ec2-54-215-138-196.us-west-1.compute.amazonaws.com'
-  },
-  secret: '076ee61d63aa10a125ea872411e433b9'
-};
-
